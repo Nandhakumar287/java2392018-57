11f70eed9e8c6f8380bf42d898e29e8a7d37108a
Nandha kumar S <mailstonandhakumar@gmail.com>
2018-09-23 02:15:51 +0530

    Update and rename README.md to Div.java

---
+++ Div.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+  public class Div{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("A Value:");
+      int a=sc.nextInt();
+      System.out.println("B Value:");
+      int b=sc.nextInt();
+      System.out.println("C Value:");
+      int c=sc.nextInt();
+      System.out.println("(A*B)/c="+((a*b)/c));
+      }
+    }  
+      
